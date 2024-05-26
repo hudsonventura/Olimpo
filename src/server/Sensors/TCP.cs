89fe17e80794c5fab6539aeae7c5504a99e2f5fe
Hudson Ventura <hudsonventura@outlook.com>
2024-05-26 11:46:17 -0400

    Generic TCP ok

--- src/server/Sensors/TCP.cs
+++ src/server/Sensors/TCP.cs
@@ -4,50 +4,55 @@ using Olimpo.Domain;
 
 namespace Olimpo.Sensors;
 
-public class TCP : ISensor
+public class TCP : ISensor3
 {
-
-    public List<Channel> GenChannels(Sensor sensor)
-    {
-        return null;
-    }
-
-    public string GetUnit()
-    {
-        return "ms";
-    }
     
-    public async Task<Metric> Test(Service service, Sensor sensor)
+    public async Task<List<Channel>> Test(Service service, Sensor sensor)
     {
         var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+        Metric metric = null;
         try
         {
             using (TcpClient client = new TcpClient())
             {
-                string host = (service.host != null) ? service.host : "localhost";
+                string host = (service.host != null) ? service.host : service.host;
 
                 var connectTask = client.ConnectAsync(service.host, (int)sensor.port);
                 var timeoutTask = Task.Delay(sensor.timeout); // Timeout of 5 seconds
                 var completedTask = await Task.WhenAny(connectTask, timeoutTask);
                 if (completedTask == timeoutTask)
                 {
-                    return new Metric() { message = $"The port {sensor.port} is not opened or a timeout was got"};
+                    metric = new Metric() { message = $"The port {sensor.port} is not opened or a timeout was got"};
                 }
 
                 var isPortOpen = client.Connected;
-                if (!isPortOpen)
+                if (isPortOpen)
                 {
-                    return new Metric() { message = $"The port {sensor.port} is not opened" };
+                    metric = new Metric() { message = "Success", latency = stopwatch.ElapsedMilliseconds , value = stopwatch.ElapsedMilliseconds };
                 }
-                return new Metric() { message = "Success", latency = stopwatch.ElapsedMilliseconds , value = stopwatch.ElapsedMilliseconds };
+                else
+                {
+                    metric = new Metric() { message = $"The port {sensor.port} is not opened or a timeout was got" };
+                }
             }
         }
         catch(Exception error)
         {
-            return new Metric(){ message = error.Message};
+            metric = new Metric(){ message = error.Message};
         }
         finally{
             stopwatch.Stop();
         }
+
+        List<Channel> channels = new List<Channel>();
+        channels.Add(
+            new Channel(){
+                name = $"{sensor.name} - Generic TCP",
+                channel_id = 1,
+                current_metric = metric,
+                unit = "ms"
+            }
+        );
+        return channels;
     }
 }
