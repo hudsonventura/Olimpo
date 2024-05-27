f60fe5cd1163c1842865417fe441584c97e5ea74
Hudson Ventura <hudsonventura@outlook.com>
2024-05-27 11:11:54 -0400

    Docker ok

--- src/server/Sensors/SSH_Docker_Container.cs
+++ src/server/Sensors/SSH_Docker_Container.cs
@@ -10,18 +10,7 @@ public class SSH_DOCKER_CONTAINER : ISensor3
     public async Task<List<Channel>> Test(Service service, Sensor sensor)
     {
         List<Channel> channels = new List<Channel>();
-        List<(string, Channel)> commands = new List<(string, Channel)>();
-        commands.Add(($"sudo -S docker stats torrent --no-stream --format \"{{{{.MemUsage}}}}\" | awk '{{print $1}}'", 
-            new Channel(){
-                name = $"{sensor.name} - Memory Used",
-                channel_id = 1,
-        }));
-        commands.Add(($"sudo -S docker stats torrent --no-stream --format \"{{{{.MemUsage}}}}\" | awk '{{print $3}}'", 
-            new Channel(){
-                name = $"{sensor.name} - Memory total",
-                channel_id = 1,
-        }));
-
+        string command = $"sudo -S docker stats torrent --no-stream";
 
         var connectionInfo = new Renci.SshNet.ConnectionInfo(service.host, sensor.username,
                 new PasswordAuthenticationMethod(sensor.username, sensor.password));
@@ -41,61 +30,41 @@ public class SSH_DOCKER_CONTAINER : ISensor3
                     string prompt = stream.Expect(new Regex(@"[$>]"));
                     //Console.WriteLine(prompt);
 
-                    foreach (var command in commands)
+                    // Send command and expect password or user prompt
+                    stream.WriteLine(command);
+                    prompt = stream.Expect(new Regex(@"([$#>:])"));
+                    //Console.WriteLine(prompt);
+
+                    // Check to send password
+                    if (prompt.Contains(":"))
                     {
-                        stream.WriteLine(command.Item1);
-                        prompt = stream.Expect(new Regex(@"([$#>:])"));
-                        //Console.WriteLine(prompt);
-
-                        // Check to send password
-                        if (prompt.Contains(":"))
-                        {
                             // Send password
-                            stream.WriteLine(sensor.password);
-                            prompt = stream.Expect(new Regex(@"[$#>]"));
-                            //Console.WriteLine(prompt);
-                        }
+                        stream.WriteLine(sensor.password);
+                        prompt = stream.Expect(new Regex(@"[$#>]"));
+                        //Console.WriteLine(prompt);
+                    }
 
-                        var parts = prompt.Trim().Split("\r\n");
-                        decimal value = 0;
-                        string unit = "";
-                        foreach (var part in parts)
-                        {
-                            try
-                            {
-                                string numberWithoutComma = part.Replace(".", ",").Replace("KiB", "").Replace("MiB", "").Replace("GiB", "").Replace("TiB", "");
-                                value = decimal.Parse(numberWithoutComma);
+                    var lines = prompt.Trim().Split("\r\n");
+                    var parts = lines[1].Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
 
-                                if(part.Contains("KiB"))
-                                    unit = "KB";
-                                if(part.Contains("MiB"))
-                                    unit = "MB";
-                                if(part.Contains("GiB"))
-                                    unit = "GB";
-                                if(part.Contains("TiB"))
-                                    unit = "TB";
-                                
-                                break;
-                            }
-                            catch (System.Exception)
-                            {
-                                
-                            }
-                        }
-
-                        for (int i = 0; i < sensor.channels.Count; i++)
-                        {
-                            sensor.channels[i].current_metric = new Metric(){
+                    //value, name, unit
+                    List<(decimal, string, string)> elements = DecodeParts(parts);
+                    
+                    for (int i = 0; i < elements.Count(); i++)
+                    {
+                        channels.Add(new Channel(){
+                                channel_id = i+1,
+                                name = $"{sensor.name} - {elements[i].Item2}",
+                                unit = elements[i].Item3,
+                                current_metric = new Metric(){
                                 latency = stopwatch.ElapsedMilliseconds,
                                 message = "Ok",
-                                //value = convertStringToDecimalInBytes(values[i])
-                            };
-                        }
-                        
+                                value = elements[i].Item1
+                            }
+                        });
                     }
-                    // Send command and expect password or user prompt
-                    
-                    //return sensor;
+
+
                 }
                 else
                 {
@@ -104,30 +73,52 @@ public class SSH_DOCKER_CONTAINER : ISensor3
             }
             catch (Exception ex)
             {
-                foreach (var command in commands)
-                {
-                    command.Item2.current_metric = new Metric(){
-                        latency = stopwatch.ElapsedMilliseconds,
-                        error_code = 1,
-                        message = $"Exception: {ex.Message}",
-                    };
-                }
+                throw;
             }
             finally
             {
                 client.Disconnect();
             }
         }
-        
-        foreach (var command in commands)
-        {
-            channels.Add(command.Item2);
-        }
 
-        
+
         return channels;
     }
 
 
+    private List<(decimal, string, string)> DecodeParts(string[] parts)
+    {
+        //value, name, unit
+        // 3 - used mememoruy
+        // 5 =- memory total
+        // 7 - dados baixados na rede
+        // 9 - dados enviados na rede
+        List<(decimal, string, string)> result = new List<(decimal, string, string)>();
 
+        result.Add((
+            decimal.Parse(parts[3].Replace(".", ",").Replace("KiB", "").Replace("MiB", "").Replace("GiB", "").Replace("TiB", "")),
+            "Used memory",
+            DecodeUnit(parts[3])
+        ));
+        
+        result.Add((
+            decimal.Parse(parts[5].Replace(".", ",").Replace("KiB", "").Replace("MiB", "").Replace("GiB", "").Replace("TiB", "")),
+            "Total memory",
+            DecodeUnit(parts[5])
+        ));
+
+        return result;
+    }
+
+    private string DecodeUnit(string unit){
+        if(unit.Contains("KiB"))
+            return "KB";
+        if(unit.Contains("MiB"))
+            return "MB";
+        if(unit.Contains("GiB"))
+            return "GB";
+        if(unit.Contains("TiB"))
+            return "TB";
+        return "B";
+    }
 }
