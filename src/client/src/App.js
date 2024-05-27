55e7613a27ac3fb97fc584c22e09f48bb919902d
Hudson Ventura <hudsonventura@outlook.com>
2024-05-27 17:14:12 -0400

    PrimeFaces installed

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 
+import 'primereact/resources/themes/lara-light-cyan/theme.css';
+import { Button } from 'primereact/button';
+
 function App() {
   return (
     <div className="App">
@@ -9,6 +12,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <Button label="PrimeReact" />
+
         <a
           className="App-link"
           href="https://reactjs.org"
